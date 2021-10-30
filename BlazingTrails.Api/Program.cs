1d9d40a051e4fac53bb9040f5bfe67b3eba04ac1
Sean Schwabenlander <sean@schwabenlander.com>
2021-10-30 14:51:41 -0500

    Added api and shared projects to the solution

---
+++ BlazingTrails.Api/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles(); 
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapFallbackToFile("index.html");
+});
+
+app.Run();
---
+++ BlazingTrails.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace BlazingTrails.Shared;
+public class Class1
+{
+
+}
